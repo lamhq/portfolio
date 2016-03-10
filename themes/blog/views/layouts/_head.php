542ff227e3abed99c6838442a1b49a9bd834ef1f
albert <albert@gmail.com>
2016-03-10 09:32:35 +0700

    revert test

--- themes/blog/views/layouts/_head.php
+++ themes/blog/views/layouts/_head.php
@@ -5,18 +5,14 @@ use yii\helpers\Html;
 /* @var $content string */
 ?>
 <head>
-	<meta charset="<?= Yii::$app->charset ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="SKYPE_TOOLBAR" content ="SKYPE_TOOLBAR_PARSER_COMPATIBLE">
-    <meta name="description" content="">
-    <meta name="author" content="">
+	<meta charset="<?= Yii::$app->charset ?>" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="SKYPE_TOOLBAR" content ="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
 	
-	<meta property="og:image" path="/blog/media/post/8/1457511385-business-money-pink-coins-large.jpg" />
-	<meta property="og:title" path="Buying a resale flat in Singapore " />
-	<meta property="og:url" path="http://onehome.sg/blog/post/8/buying-a-resale-flat-in-singapore" />
-
-<link rel="icon" href="favicon.ico">
+	<link rel="icon" href="favicon.ico"/>
     
 	<?= Html::csrfMetaTags() ?>
 	<title><?= Html::encode($this->title) ?> - <?= Html::encode(Yii::$app->name) ?></title>
