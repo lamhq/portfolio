e8e0aa751eee7300cae85d2d4f5ee0d96b8503b0
John Doe <johndoe@example.com>
2016-03-13 14:46:01 +0700

    create portfolio branch

--- protected/data/import.bat
+++ protected/data/import.bat
@@ -1,4 +1,4 @@
-d:\program\mysql\bin\mysql -u root -e "drop database myapp"; 
-d:\program\mysql\bin\mysql -u root -e "create database myapp DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_unicode_ci;";
-d:\program\mysql\bin\mysql -u root myapp < data.sql
+d:\program\mysql\bin\mysql -u root -e "drop database portfolio"; 
+d:\program\mysql\bin\mysql -u root -e "create database portfolio DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_unicode_ci;";
+d:\program\mysql\bin\mysql -u root portfolio < data.sql
 
