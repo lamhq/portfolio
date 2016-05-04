69250f5a87afd991c3d725ec70ed19d492e47f64
albert <albert@gmail.com>
2016-05-04 14:01:16 +0700

    fix bug caused by changing table prefix

--- protected/modules/portfolio/controllers/ProjectController.php
+++ protected/modules/portfolio/controllers/ProjectController.php
@@ -11,7 +11,7 @@ class ProjectController extends Controller {
 		$query = Project::find()->where(['status'=>  Project::STATUS_ACTIVE]);
 		if ($tag) {
 			$query->innerJoinWith(['tags'=> function($query) use ($tag) {
-				$query->andWhere('blog_tag.slug LIKE :tag', [':tag'=>$tag]);
+				$query->andWhere('my_tag.slug LIKE :tag', [':tag'=>$tag]);
 			}], true);
 		}
 		
