b7a26eb4dbcf43b747f3bc9e1ee7111fcc4d93c8
Lam <daibanglam@gmail.com>
2017-05-23 09:48:42 +0700

    change to dev

--- protected/config/main.php
+++ protected/config/main.php
@@ -147,11 +147,11 @@ $config = [
 
 if (YII_DEBUG) {
 	// configuration adjustments for 'dev' environment
-	$config['bootstrap'][] = 'debug';
-	$config['modules']['debug'] = 'yii\debug\Module';
+	// $config['bootstrap'][] = 'debug';
+	// $config['modules']['debug'] = 'yii\debug\Module';
 
-	$config['bootstrap'][] = 'gii';
-	$config['modules']['gii'] = 'yii\gii\Module';
+	// $config['bootstrap'][] = 'gii';
+	// $config['modules']['gii'] = 'yii\gii\Module';
 }
 
 return $config;
